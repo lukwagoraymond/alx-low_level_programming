0f92f9497978d091b25131c68425777b22564883
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-06-30 20:19:02 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - prints the string suing printf
+* Return: 0 if the program is successful
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
