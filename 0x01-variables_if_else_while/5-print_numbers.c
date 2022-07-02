8b5378aebb6333ec4a3b04bae8513d7593712318
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-01 19:48:56 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Function used as an entry point
+* Return: Always returns zero when programme terminates successfully
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
