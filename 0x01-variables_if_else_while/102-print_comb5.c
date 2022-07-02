41dd864c7c3b606ee6c13b722a3421af54255cda
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-01 21:51:05 -0700

    Task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+* main - Function used as an entry point
+* Return: Always returns zero when programme terminates successfully
+*/
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 <= 98; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= 99; num2++)
+		{
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 98 && num2 == 99)
+				continue;
+
+			putchar(44);
+			putchar(32);
+		}
+	}
+
+	putchar(10);
+
+	return (0);
+}
