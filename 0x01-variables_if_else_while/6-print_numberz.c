90098ed78125d45a7741a2aa6fe00aa4bd876c94
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-01 19:55:55 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Function used as an entry point
+* Return: Always returns zero when programme terminates successfully
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
