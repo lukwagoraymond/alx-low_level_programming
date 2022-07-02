106582768e06cb85c6c671c1ffceb81da5a2d70b
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-01 19:29:52 -0700

    Task

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,16 +7,14 @@
 int main(void)
 {
 	int i;
-	int j;
 
 	for (i = 97; i <= 122; i++)
 	{
-		putchar(i);
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+		putchar(10);
 	}
-	for (j = 65; i <= 90; j++)
-	{
-		putchar(j);
-	}
-	putchar(10);
 	return (0);
 }
