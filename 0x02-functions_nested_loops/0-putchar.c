e7f63abc9744e9b8d8a5167018deb96859f8cc40
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-05 16:33:32 +0300

    New Task(0)

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* main - Main entry function to to body of code
+* Return: 0 if the code is successful. 1 for otherwise
+*/
+
+int main(void)
+{
+    _putchar(95);
+    _putchar(112);
+    _putchar(117);
+    _putchar(116);
+    _putchar(99);
+    _putchar(104);
+    _putchar(97);
+    _putchar(114);
+    _putchar(10);
+
+    return(0);
+}
