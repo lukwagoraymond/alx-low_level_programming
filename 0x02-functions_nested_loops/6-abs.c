ed52658ac6b810d1c6c046eff0c3d70fa9894135
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-05 12:19:03 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _abs - Computes absolute number of integer
+* @n: The interger to be checked
+*
+* Return: If n then absolute
+*/
+int _abs(int)
+{
+	if (n <= 0 || n >= 0)
+		return (n);
+	else
+		return (-n);
+}
