e121b8465b2906f9139079e1568d8fcd90e88a33
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-05 10:19:21 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+/* main.h includes function prototype*/
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		int j;
+
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+	_putchar(10);
+	}
+
+}
+
