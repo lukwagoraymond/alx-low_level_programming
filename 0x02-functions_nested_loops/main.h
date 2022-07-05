733d4be5412f27ff93bfc24849052463f6c7508f
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-05 09:13:59 -0700

    Task 1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,10 @@
 /**
  * _putchar - writes the character c to stdout
+ * void print_alphabet(void) - prints the alphabet in lowercase 
  * @c: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c);
+void print_alphabet_x10(void);
