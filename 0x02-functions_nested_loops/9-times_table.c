4d7fb61acc2c5d5e8935d6341f933ce754cff322
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-05 14:28:26 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,20 +5,16 @@
 */
 void times_table(void)
 {
-	int i;
+	int i, m, pd;
 
 	for (i = 0; i <= 9; i++)
 	{
 		_putchar('0');
 
-		int m;
-
 		for (m = 1; m <= 9; m++)
 		{
 			_putchar(44);
 			_putchar(32);
-
-			int pd;
 
 			pd = i * m;
 
