0881513c1a7baabff9147d622b1b0c723f0764ad
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-07 21:29:45 +0300

    Task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,12 +16,11 @@ void print_diagonal(int n)
 			for (k = 0; k < i; k++)
 				_putchar(32); /* ASCII for Space Bar */
 			_putchar(92); /* ASCII for Black lash */
-			if (k == n - 1)
+			if (i == n - 1)
 				continue;
 			_putchar(10); /* ASCII for New line */
 		}
 	}
 	_putchar(10);
-
 }
 
