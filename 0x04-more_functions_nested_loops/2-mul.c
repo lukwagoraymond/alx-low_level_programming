fad21f952453cbc8ebdd60bf01247ef4978c2fa6
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-07 17:30:37 +0300

    Task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,17 @@
+/* Function to multiple two integers*/
+/* Author: Raymond Lukwago */
+
 #include "main.h"
 /**
- * _isupper - checks for digit
- *
- * @n: The digit to be checked
- * Return: Always 0 if successful otherwise 0
- */
- int _isdigit(int c);
+* _isupper - checks for digit
+*
+* @n: The 1st of the two integers
+* @b: The 2nd of the two integers
+* Return: i for the multiple of two integers
+*/
+int mul(int a, int b)
  {
- 	if((c >= 48) && (c <= 57))
- 		return (1);	
- 	else 
- 		return (0);
+ 	int c;
+ 	c = (a * b);	
+ 	return (c);
  }
