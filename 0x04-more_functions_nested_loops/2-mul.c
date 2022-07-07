937312a47d7c9729608badf53582433f8dad9f70
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-07 16:28:50 +0300

    New Project

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ *
+ * Return: Always 0 if successful otherwise 0
+ */
+ int _isupper(int c)
+ {
+ 	if((c >= 65) && (c <= 90))
+ 		return (1);	
+ 	else 
+ 		return (0);
+ }
