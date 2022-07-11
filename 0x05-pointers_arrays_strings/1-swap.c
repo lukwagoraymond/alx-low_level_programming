09ab7c707757698054b8d12b28884e4beb9ba082
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-11 20:59:28 +0300

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* swap_int - Swap two numbers
+* @a: Pointer variable of one number
+* @b: Pointer variable of two number
+*/
+void swap_int(int *a, int *b)
+{
+	int i;
+
+	a = &i;
+	b = a;
+
+}
