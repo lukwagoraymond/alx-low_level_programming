d5dbf664a2cf592223a3475ef5494a4e3a3edb7f
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-11 22:03:23 +0300

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_rev - prints string in reverse
+* @s: The string to be printed
+*/
+void print_rev(char *s)
+{
+	int len, i;
+
+	len = 0;
+
+	while (s[i++])
+		len++;
+
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar(10);
+}
