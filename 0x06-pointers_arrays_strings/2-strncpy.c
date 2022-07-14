c5564b3d53297c6b9d341f352829a897c82cc1d2
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-14 16:56:56 +0300

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,10 +12,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
+	int length;
+
+	while (src[length])
+		length++;
+
 	for (i = 0; i < n && src[i]; i++)
 		dest[i] = src[i];
 
-	for ( ; i < n; i++)
+	for (i = length; i < n; i++)
 		dest[i] = '\n';
 
 	return (dest);
