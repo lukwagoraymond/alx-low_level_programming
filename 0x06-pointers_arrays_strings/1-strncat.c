2fc8f3a324ed7442689e50d64fc4b4ba0c88cc5c
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-14 15:19:45 +0300

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+/* Author: Raymond Lukwago A.R */
+
+#include "main.h"
+
+/**
+* _strncat - concatenates two strings
+* @src: String to be appended
+* @dest: String to be appended to
+* @n: number of bytes from src string
+* Return: Returns a string joined together
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int length = 0;
+
+	int i;
+
+	while (dest[length])
+		length++;
+
+	for (i = 0; i < n; &&src[i] != '\0'; i++)
+	{
+		dest[length] = src[i];
+		length++;
+	}
+
+	dest[length] = '\0';
+
+	return (dest);
+}
