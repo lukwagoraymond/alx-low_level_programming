7e76be21709fc97c2d669003b166f365bee6ab76
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-14 19:04:19 +0300

    Task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,15 @@
+/* Author: Raymond Lukwago A.R */
+#include "main.h"
+
+/**
+* reverse_array - Reverses an integer array
+* @a: Pointer to the array to be reversed
+* @n: Size of the array to be reversed
+*/
+void reverse_array(int *a, int n)
+{
+	int i;
+	
+	for ( i = n - 1; i > 0; i--)
+		a[i];
+}
