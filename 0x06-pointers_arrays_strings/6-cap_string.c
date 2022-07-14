fcd18c13803a87901a3004279536726b5644d079
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-14 21:56:47 +0300

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+/* Author: Raymond Lukwago A.R */
+#include "main.h"
+
+/**
+* cap_string - Capitalises under specific conditions
+* @n: String to be changed
+* Return: Character string converted to uppercase
+*/
+char *cap_string(char *n)
+{
+	char cat[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	int length = 13; /* length of the cat[] */
+	int a; /* size of the n[] string */
+	int index; /* index to loop check for cat[] in n[] */
+
+	for (a = 0; n[a]; a++)
+	{
+		for (index = 0; index < length; a++)
+		{
+			if ((a == 0 || n[a - 1] == cat[i]) && (x[a] >= 97 && n[a] <= 122))
+				n[a] -= 32;
+		}
+	}
+	return (n);
+}
