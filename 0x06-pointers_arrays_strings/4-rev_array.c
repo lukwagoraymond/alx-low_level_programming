ca5285a0f6f62628318eaafd1eb2a9a12acc384c
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-14 20:01:17 +0300

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,8 +8,13 @@
 */
 void reverse_array(int *a, int n)
 {
+	int tmp[n];
 	int i;
+	
+	for (i = 0; i < n; i++)
+		tmp[n - 1 - i] = a[i];
+		
+	for (i = 0; i < n; i++)
+		a[i] = tmp[i];
 
-	for (i = n - 1; i > 0; i--)
-		a[i];
 }
