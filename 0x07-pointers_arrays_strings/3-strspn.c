f34c2ed99d81fbe396689303757fbb9463f5236b
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-18 19:23:46 +0300

    Task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+/* Author: Raymond Lukwago */
+
+#include "main.h"
+/**
+* _strspn - gets the length of a prefix substring
+* @s:	String to be compared to
+* @accept:	string of bytes to compare from
+* Return:	Number of bytes similar among (2)
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0' && s[i] != accept[j]; j++)
+
+		if (accept[j] == '\0')
+			return (i);
+	}
+
+	return (0);
+}
