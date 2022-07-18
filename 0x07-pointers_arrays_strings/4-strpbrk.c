7045298082a8d8709563327890b49d52d378ba1e
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-18 20:04:17 +0300

    Task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,15 +9,17 @@
 */
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
+	int j;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s != '\0')
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 				return (s);
 		}
+
+		s++;
 	}
 
 	return (0);
