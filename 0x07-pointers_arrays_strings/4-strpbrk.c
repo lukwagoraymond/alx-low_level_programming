bfea07fcd9930afeee480ab33c69d0e4dc100d9f
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-18 19:59:25 +0300

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+/* Author: Raymond Lukwago */
+
+#include "main.h"
+/**
+* _strpbrk - Finds 1st character matching
+* @s:	String to be compared to
+* @accept:	string of bytes to compare from
+* Return:	Pointer to character in s matching accept
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (s);
+		}
+	}
+
+	return (0);
+}
