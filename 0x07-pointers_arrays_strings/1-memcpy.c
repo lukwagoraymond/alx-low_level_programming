112bbafa467434a2bee7cc684c2cf1dad11be7b0
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-18 17:48:54 +0300

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,12 +10,12 @@
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int index; 
-	
+	unsigned int index;
+
 	for (index = 0; index < n; index++)
 	{
 		*(dest + index) = *(src + index);
 	}
-	
+
 	return (dest);
 }
