e80f3440349e21d28c498d74a3b515b50d94572e
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-18 21:37:14 +0300

    Task 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+/* Author: Raymond Lukwago */
+
+#include "main.h"
+/**
+* print_chessboard - Prints ChessBoard
+* @a:	Number of Elements in Rows
+*/
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+	}
+	_putchar(10);
+}
