450781fedb9ba513f1d862e069295c8823fd72ba
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-18 23:39:13 +0300

    Task 8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,29 @@
+/* Author: Raymond Lukwago */
+
+#include "main.h"
+/**
+* print_diagsums - Prints Diagonals of Matrix
+* @size: Size of the matrix
+* @a:	Pointer to 2D Matrice
+*/
+void print_diagsums(int *a, int size)
+{
+	int principal = 0;
+	int secondary = 0;
+	int i;
+	int j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+				principal += *(*a + i) + j;
+
+			if ((i + j) == (n - 1))
+				secondary += *(*a + i) + j;
+		}
+	}
+	printf("%d, %d\n", principal, secondary);
+}
+
