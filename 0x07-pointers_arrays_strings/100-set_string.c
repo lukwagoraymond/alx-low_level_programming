79dc9caae0ae6fae736ab871dd2e3e8ce5c24e78
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-19 12:03:03 +0300

    Task 9

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -4,7 +4,7 @@
 /**
 * set_string - converts string of s
 * @s: Pointer (s) pointing to address of to
-* @to:	Pointer (to) pointing to address of 
+* @to:	Pointer (to) pointing to address of
 *		string value
 */
 void set_string(char **s, char *to)
