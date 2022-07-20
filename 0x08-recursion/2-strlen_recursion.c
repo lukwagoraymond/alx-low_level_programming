826d248175dbdb39df839cbd5834ac0a87286201
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-20 19:01:12 +0300

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+/* Author: Raymond Lukwago */
+
+#include "main.h"
+/**
+* _strlen_recursion - Prints length of string
+* @s: Pointer to string length
+* Return: Returns the length of string
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1))
+}
