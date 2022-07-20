f9af7bbe6e371f21dfe77eb66c1a75befbe4e9cf
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-20 17:46:13 +0300

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+/* Author: Raymond Lukwago */
+
+#include "main.h"
+/**
+* _puts_recursion - Prints string with newline at end
+* @s: Pointer to string to be printed to stdout
+*/
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar(10);
+
+	_putchar(*s);
+	_puts_recursion(s++);
+}
