f9c4c44eb775446807a3bc210304aa2094a0d54f
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-20 23:32:29 +0300

    Task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+/* Author: Raymond Lukwago */
+
+#include "main.h"
+
+int sqrt_test(int x, int g);
+/**
+* _sqrt_recursion - Calculates square root of number
+* @n:		Base integer of the number
+* Return:	Returns Calculated sqrt integer of number
+*			or a (-1) for non square numbers
+*/
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	return (sqrt_test(n, 1));
+}
+
+/**
+* sqrt_test -	Tests if guessed number is close to
+*				division (Number / guessed number)
+* @x:			Base number to determine square root
+* @g:			Guessed Number of the square root of
+*				number (x)
+* Return:		Returns closest guessed number to
+*				divsion (Number / guessed number)
+*/
+int sqrt_test(int x, int g)
+{
+	if (g == (x / g))
+		return (g);
+
+	if (g > (x / g)
+		return (-1);
+
+	return (sqrt_test(g + 1, x));
+}
