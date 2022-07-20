a2d1df1425bfd9983112550283b59cde8072a4d2
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-21 00:25:11 +0300

    Task 6

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,5 +8,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int sqrt_test(int x, int g);
+int is_prime_number(int n);
+int is_divisible(int num, int div);
 #endif
 
