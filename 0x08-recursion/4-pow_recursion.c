dbc356998b9fe2cd8a029e87c8dd4f0a39561110
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-20 20:25:45 +0300

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+/* Author: Raymond Lukwago */
+
+#include "main.h"
+/**
+* _pow_recursion - Calculates number to certain power
+* @x: Base integer of the number
+* @y: The power to calculate to
+* Return: Returns Calculated power integer of number
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y > 0)
+		return (x * _pow_recursion(x, (y - 1)));
+
+	else if (y == 0)
+		return (1);
+
+	else if (y < 0)
+		return (-1);
+}
