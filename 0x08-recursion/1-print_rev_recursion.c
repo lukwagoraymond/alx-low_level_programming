669a1050216c91fb2f30ba0605d1d859ad3e9b5e
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-20 19:32:08 +0300

    Task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+/* Author: Raymond Lukwago */
+
+#include "main.h"
+/**
+* _print_rev_recursion - Prints string in reverse
+* @s: Pointer to string to be printed to stdout
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar(10);
+		return;
+	}
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+
+}
+
