c2ae107cc92d57b2b5677c950a2008fb01778b86
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-26 14:13:15 +0300

    Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+/* Author: Raymond Lukwago */
+
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _strdup -	Creates a copy of original string
+* @str:		String to be copied
+* Return:	Returns pointer to copy of (str)
+*/
+char *_strdup(char *str)
+{
+	int len;
+	int i;
+	char ptrS;
+
+	while (*str++)
+		len++;
+
+	if (len == 0)
+		return (NULL);
+
+	ptrS = malloc(len * (sizeof(char));
+
+	if (ptrS == NULL)
+		return (NULL);
+
+	else
+		for (i = 0; i = *(str + i); i++)
+			*(ptrS + i) = *(str + i);
+
+	return (ptrS);
+}
