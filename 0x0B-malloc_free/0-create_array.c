d00bcc8c830fcfad3fad5457280aaaeee56609dc
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-26 13:34:16 +0300

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,26 +7,26 @@
 * create_array -	Creates an array of characters 'c'
 * @size:			Length of the array to be created
 * @c:				Character to be put in array
-* Return:			Returns pointer to an array		
+* Return:			Returns pointer to an array
 */
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *ptrA;
-	
+
 	if (size <= 0) /* validate size input */
 		return (NULL);
 
 	ptrA = malloc(size * sizeof(char)); /* Allocate Memory */
-	
+
 	if (ptrA == 0)	/* Validate Memory */
-		return(NULL);
+		return (NULL);
 
 	else
 
 		for (i = 0; i <= size; i++)
 			*(ptrA + i) = c;
-	
-	return(ptrA);
+
+	return (ptrA);
 
 }
