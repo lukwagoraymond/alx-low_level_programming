b55f919acc9a2341208c1df7b1f94144bc84bf34
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-07-28 20:52:22 +0300

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,12 +32,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	len = 0;
 
 	for (i = 0; s1[i]; i++)
-		*(concat + len) = *(s1 + i);
-		len++;
+		concat[len++] = *(s1 + i);
 
 	for (i = 0; s2[i] && i < n; i++)
-		*(concat + len)  = *(s2 + i);
-		len++;
+		concat[len++]   = *(s2 + i);
 
 	*(concat + len) = '\0';
 
