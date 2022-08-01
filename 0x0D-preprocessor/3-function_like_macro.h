dc671078a9b193443d5c1151b3899d0e28d13961
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-08-01 20:06:11 +0300

    Task 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef FUNCTION_LIKE_MARCO
 #define FUNCTION_LIKE_MARCO
 
-#define ABS(x) {((x) < 0) ? -(x) : (x)}
+#define ABS(x) (((x) < 0) ? -(x) : (x))
 
 #endif /* ifndef FUNCTION_LIKE_MARCO */
