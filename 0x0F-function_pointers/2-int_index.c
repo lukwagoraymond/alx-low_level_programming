7317f8b106f59b76f1d79ce47d773198b063fbb3
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-08-03 22:51:51 +0300

    Task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,10 +15,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp == NULL || size <= 0)
+	if (!(array) || !(cmp) || size <= 0)
 		return (-1);
 
 	for (i = 0; i < size; i++)
+	{
 		if (cmp(array[i]) != 0)
 			return (i);
+	}
 }
