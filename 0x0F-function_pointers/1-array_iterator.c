6fe0bb5ec6ab49eeaec0071b7e37368250e7a2d6
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-08-03 21:59:59 +0300

    Task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,14 +11,12 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t = i;
-	i = 0;
-	
+
 	if (action != NULL && array != NULL)
 	{
-		while (i < size)
+		for (i = 0; i < size; i++)
 		{
 			action(array[i]);
-			i++;
 		}
 	}
 }
