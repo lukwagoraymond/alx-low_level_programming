6a182e9b3dc0751e5fbb67d2d3beeecdf99d94a6
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-08-11 23:05:19 +0300

    Task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - print all element of a linked list
+ * @h: head of the linked list
+ *
+ * Return: number of element in the linked list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t len = 0;
+
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
