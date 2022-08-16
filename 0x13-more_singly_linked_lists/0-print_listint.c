1086b1f3e07d96b9fb0bce1ab88a2b5adc91233f
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-08-16 21:02:05 +0300

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,11 +1,11 @@
 /* Author:	Raymond Lukwago A.R */
 #include "lists.h"
-#include <stdarg.h>
+#include <stdio.h>
 
 /**
 * print_listint -	Prints all elements of a linked list
-* @h:				Pointer to the address of the first node
-* Return:			Returns the number of nodes
+* @h:			Pointer to the address of the first node
+* Return:		Returns the number of nodes
 */
 size_t print_listint(const listint_t *h)
 {
@@ -13,11 +13,7 @@ size_t print_listint(const listint_t *h)
 
 	while (h)
 	{
-		if (h == NULL)
-			printf("No list to be printed\n");
-		else
-			printf("%d", h->n);
-
+		printf("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
