38ce02dc774102c5e91b7f5ba563af3b6dda367c
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-08-16 21:14:24 +0300

    Task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,5 +1,6 @@
 /* Author:	Raymond Lukwago A.R */
 #include "lists.h"
+#include <stdlib.h>
 
 /**
 * add_nodeint -	Adds a node at the beginning of a linked list
@@ -20,6 +21,12 @@ listint_t *add_nodeint(listint_t **head, const int n)
 		return (NULL);
 
 	new_node->n = n;
+
+	if (new_node->n == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
 	new_node->next = *head;
 	*head = new_node;
