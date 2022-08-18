7471938bc5d9398137e35f0f19f5974dea850f34
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-08-18 23:26:00 +0300

    Task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,12 +12,12 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit;
 	unsigned long int max_bits;
-	
+
 	/*validate index if not out of range*/
 	max_bits = ((sizeof(unsigned long int)) * 8);
 	if (index > max_bits)
 		return (-1);
-		
+
 	bit = ((n >> index) & 1);
 	return (bit);
 }
