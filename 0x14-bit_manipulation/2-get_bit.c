b651b9b337898ca64b8720d119cd9e83e1853b81
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-08-18 23:24:49 +0300

    Task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,23 @@
+/* Author: Raymond Lukwago */
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* get_bit -	Gets bit at particular index
+* @n:		number to be searched
+* @index:	Position of the bit being searched
+* Return:	Bit (1) or (0) else (-1) for error
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit;
+	unsigned long int max_bits;
+	
+	/*validate index if not out of range*/
+	max_bits = ((sizeof(unsigned long int)) * 8);
+	if (index > max_bits)
+		return (-1);
+		
+	bit = ((n >> index) & 1);
+	return (bit);
+}
