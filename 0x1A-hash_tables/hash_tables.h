752e3b4a914feb45824aba7ed9661824bad5936e
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-10-22 01:31:42 +0300

    Updated Declarations

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -42,5 +42,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 
+hash_node_t *h_n_create(const char *key, const char *value);
+void free_list(hash_node_t *head);
 
 #endif
