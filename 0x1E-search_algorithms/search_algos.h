a4b8731812573161c488d07819e8b6c32b372a4d
Raymond Lukwago <lukwagoraymond3@gmail.com>
2023-01-29 20:10:48 +0300

    Header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef SEARCH_ALGO_H
+#define SEARCH_ALGO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int recursive_search_binary(int *array, size_t low, size_t high, int value);
+void print_array(int *array, size_t low, size_t high);
+
+#endif /* SEARCH_ALGO_H */
